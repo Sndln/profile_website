59609c20a9acb3f84e7b924a7a817fd82156e2e9
Mirza <sandeleen.mirza@capgemini.com>
2022-06-24 14:08:56 +0200

    init

---
+++ src/components/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="projectModal">
+            <h2 class="headerModal">{{ header }}</h2>
+            <div class="project">
+                <img @click="openProject" v-for="project in projects" :key="project" :src="project.img" :alt="project.title"> 
+            </div>
+        </div>
+    </div>
+
+    <div v-if="showProject1">
+        <Project @close="openProject"/>
+    </div>
+</template>
+
+<script>
+import Project from './Project.vue'
+
+export default {
+    components: { Project },
+    props: [ 'header', 'test' ],
+    data() {
+        return {
+            projects: [
+                { title: 'Project 1', img: require('../assets/img/photo.png') },
+                { title: 'Project 2', img: require('../assets/img/photo.png') },
+                { title: 'Project 3', img: require('../assets/img/photo.png') },
+            ],
+            showProject1: false,
+        }
+    },
+    methods:{
+        closeModal() {
+            this.$emit('close')
+        },
+        openProject() {
+            this.showProject1 = ! this.showProject1
+        }
+    }
+}
+</script>
---
+++ src/components/Project.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="projectModal">
+            <h2 class="headerModal">{{  }}</h2>
+            <div class="project">
+                <h1>{{ header }}</h1> 
+                <p>{{ text }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            header: 'Project 1',
+            text: 'lorem ipsum text',
+        }
+    },
+    methods:{
+        closeModal(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style scoped>
+p {
+    color: hotpink;
+}
+</style>
